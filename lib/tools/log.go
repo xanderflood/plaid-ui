c2a3ec4b1d79f6900ced7a232ce06059e44bb0bb
Xander Flood <xander.t.flood@gmail.com>
2019-10-09 22:16:20 -0400

    logging

---
+++ lib/tools/log.go
@@ -0,0 +1,25 @@
+package tools
+
+import (
+	"os"
+
+	"github.com/juju/loggo"
+	"github.com/juju/loggo/loggocolor"
+)
+
+//Logger exposes a minimal logging interface
+//go:generate counterfeiter . Logger
+type Logger interface {
+	Errorf(message string, args ...interface{})
+	Infof(message string, args ...interface{})
+	Warningf(message string, args ...interface{})
+	Tracef(message string, args ...interface{})
+	Debugf(message string, args ...interface{})
+}
+
+//NewStdoutLogger returns a Logger that writes colorized
+//output to stdout.
+func NewStdoutLogger() Logger {
+	loggo.RegisterWriter("stdout", loggocolor.NewWriter(os.Stdout))
+	return loggo.GetLogger("stdout")
+}
