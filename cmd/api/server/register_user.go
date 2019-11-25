2b6126df7bf8d3f9c660eea5eaa7960102c6fc65
Xander Flood <xander.t.flood@gmail.com>
2019-11-24 22:35:23 -0500

    fix the binding on the admin endpoint

--- cmd/api/server/register_user.go
+++ cmd/api/server/register_user.go
@@ -25,7 +25,7 @@ func (a ServerAgent) RegisterUser(c *gin.Context) {
 	}
 
 	var req RegistrationRequest
-	err := c.ShouldBindJSON(&req)
+	err := c.Bind(&req)
 	if err != nil {
 		//TODO is this error message safe to expose?
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
