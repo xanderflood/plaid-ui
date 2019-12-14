87b61ad7f80b1780e2f3e2d625e5988967f6786e
Xander Flood <xander.t.flood@gmail.com>
2019-12-14 18:14:18 -0500

    trying to fix equality check

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/env bash
 set +e
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
@@ -10,14 +10,14 @@ export docker_repo=xanderflood/plaid-ui
 export docker_build_directory=./build/api
 
 # build and push the base image
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "staging" ; fi`
+export TAG=`if [ "$TRAVIS_BRANCH" = "master" ]; then echo "latest"; else echo "staging" ; fi`
 export tmpname="local"
 export dockerfile="$docker_build_directory/Dockerfile"
 export tags="build-${TRAVIS_BUILD_NUMBER},commit-${TRAVIS_COMMIT::8},$TAG"
 ./build_and_push_image.sh
 
 # build and push the swarm-focused image
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "swarm"; else echo "swarm-staging" ; fi`
+export TAG=`if [ "$TRAVIS_BRANCH" = "master" ]; then echo "swarm"; else echo "swarm-staging" ; fi`
 export tmpname="swarm-local"
 export dockerfile="$docker_build_directory/swarm.Dockerfile"
 export tags="build-${TRAVIS_BUILD_NUMBER},commit-${TRAVIS_COMMIT::8},$TAG"
